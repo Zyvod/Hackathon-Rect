e21db2ce17cb23c8a5fd2786df1490eb9f0ccded
Alexander_Fields <Zyvod1300@yahoo.com>
2023-10-24 17:33:30 -0500

    Added "Tech Demo" to showcase pixi.js

--- Game/main.js
+++ Game/main.js
@@ -7,7 +7,7 @@ const app = new Application({
   antialias: true,
 });
 
-app.renderer.backgroundColor = 'black'
+app.renderer.background.color = 'slategrey'
 
 app.renderer.resize(window.innerWidth, window.innerHeight);
 
@@ -17,20 +17,13 @@ document.body.appendChild(app.view);
 
 const Graphics = PIXI.Graphics;
 
-const player1 = new Graphics();
-player1.beginFill('red')
-.lineStyle(4,'darkred',1)
-.drawRect(600,600,50,150)
-.endFill();
-
-const player2 = new Graphics();
-player2.beginFill('blue')
-.lineStyle(4,'darkblue',1)
-.drawRect(800,600,50,150)
-.endFill();
+const gravity = .9
+const sunSet = .1
+const playerMove = .15
+let hail = []
 
 const grass = new Graphics();
-grass.beginFill('darkgreen')
+grass.beginFill('snow')
 .lineStyle(2,'green',0.3)
 .drawRect(0,750,window.innerWidth,window.innerHeight)
 .endFill();
@@ -38,10 +31,47 @@ grass.beginFill('darkgreen')
 const circle = new Graphics();
 circle.beginFill('orange')
 .lineStyle(3,'yellow',0.5)
-.drawCircle(920,400,90)
+.drawCircle(920,100,90)
 .endFill();
 
-app.stage.addChild(grass)
-app.stage.addChild(circle)
+const player1 = new Graphics();
+player1.beginFill('red')
+.lineStyle(5,'darkred',1)
+.drawRect(250,600,50,150)
+.endFill();
 app.stage.addChild(player1)
+
+const player2 = new Graphics();
+player2.beginFill('blue')
+.lineStyle(5,'darkblue',1)
+.drawRect(1550,600,50,150)
+.endFill();
 app.stage.addChild(player2)
+
+app.stage.addChild(circle)
+app.stage.addChild(grass)
+
+app.ticker.add(delta => animationLoop(delta));
+
+function animationLoop(delta) {
+
+const hailFall = new Graphics();
+hailFall.beginFill('ivory')
+.drawCircle(Math.random() * app.screen.width, 0, 5,5)
+hailFall.lineStyle(2,'white',0.5)
+hail.push(hailFall)
+app.stage.addChild(hailFall)
+falling(hail)
+circle.y += sunSet
+player1.x += playerMove
+player2.x -= playerMove
+}
+
+function falling(array) {
+  for ( let i = 0; i < array.length; i++) {
+    array[i].y += gravity
+    array[i].rotation += 0.001
+  }
+}
+
+
